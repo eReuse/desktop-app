9e30a7ac635dbb539e737db3c4081e6eeba89701
Nadeu <jnadeu@gmail.com>
2017-04-16 22:21:45 +0200

    Change basic structure

---
+++ README.md
@@ -0,0 +1,25 @@
+# desktop-app
+
+> Helps to show information about eReuse and supports incidents
+
+## Dev
+
+```
+$ npm install
+```
+
+### Run
+
+```
+$ npm start
+```
+
+### Build
+
+```
+$ npm run dist
+```
+
+## License
+
+MIT © [Nadeu](https://github.com/eReuse/desktop-app)
---
+++ app/main.js
@@ -0,0 +1,38 @@
+const electron = require('electron'); //create necessary objects
+const {app,BrowserWindow} = electron;
+const path = require('path');
+const url = require('url');
+const updater = require('electron-simple-updater');
+
+let winMain //create main window
+
+updater.init('https://raw.githubusercontent.com/eReuse/desktop-app/updates.json'); //check if new version is available
+
+function createWindow() {
+	// Create browser window
+
+	winMain = new BrowserWindow({
+		width: 800,
+		height: 650
+	});
+
+	//Load index.html
+
+	winMain.loadURL('file://' + __dirname + '/index.html');
+
+	//Open devtools
+
+	winMain.webContents.openDevTools();
+
+	winMain.on('closed', () => {
+		winMain = null;
+	});
+}
+
+	//Run create windows function
+	app.on('ready', createWindow);
+
+	//Quit when all windows are closed
+	app.on('window-all-closed', () => {
+		app.quit()
+	});
