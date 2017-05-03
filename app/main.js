e0ca53c3b4bc33ee35313baa561de433490134ba
Nadeu <jnadeu@gmail.com>
2017-05-03 18:01:48 +0200

    minor changes

--- app/main.js
+++ app/main.js
@@ -1,56 +1,65 @@
-const electron = require('electron');
-const {app,BrowserWindow} = electron;
-const updater = require('electron-simple-updater');
-//const PythonShell = require('python-shell');
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+const path = require('path')
+const url = require('url')
+const updater = require('electron-simple-updater')
+// const PythonShell = require('python-shell');
 
-let winMain = null //create main window
+let winMain = null // create main window
 
-//check if new version is available
+// check if new version is available
 updater.init({
   autoDownload: true,
   checkUpdateOnStart: true,
-	url: 'https://raw.githubusercontent.com/eReuse/desktop-app/master/updates.json'
-});
+  url: 'https://raw.githubusercontent.com/eReuse/desktop-app/master/updates.json'
+})
 
-function createWindow() {
-	// Create browser window
+function createWindow () {
+  // Create browser window
 
-	winMain = new BrowserWindow({
-		show: false,
-		width: 1024,
-		height: 728
-	});
+  winMain = new BrowserWindow({
+    width: 1024,
+    height: 728,
+    minWidth: 720,
+    minHeight: 400,
+    backgroundColor: '#c3c3c3',
+    show: false
+  })
 
-	//Load index.html
+  // Load index.html
+  winMain.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
-	winMain.loadURL('file://' + __dirname + '/index.html');
-
+  // wait all are ready to show
   winMain.webContents.on('did-finish-load', () => {
-    winMain.show();
-    winMain.focus();
-  });
+    winMain.show()
+    winMain.focus()
+  })
 
-	//Open devtools
+  // Open devtools
 
-	//winMain.webContents.openDevTools();
+  // winMain.webContents.openDevTools();
 
-	winMain.on('closed', () => {
-		winMain = null;
-	});
+  winMain.on('closed', () => {
+    winMain = null
+  })
 }
 
-	//Run create windows function
-	app.on('ready', createWindow);
+// Run create windows function
+app.on('ready', createWindow)
 
-	//Quit when all windows are closed
-	app.on('window-all-closed', () => {
-		app.quit()
-	});
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+  app.quit()
+})
 
 /*
-PythonShell.run('my_script.py', function (err) {
-  if (err) {
-    throw err;
-  }
-  console.log('finished');
-});*/
+ PythonShell.run('my_script.py', function (err) {
+ if (err) {
+ throw err;
+ }
+ console.log('finished');
+ }); */
