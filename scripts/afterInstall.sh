de409ccf329465b56280cf205c2faa82f75e9e61
Nadeu <jnadeu@gmail.com>
2017-08-09 19:57:07 +0200

    install workbench

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# instalar require script.js amb npm
-# curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-
 npm install -g semver request request-promise
+
+#install workbench and requirements
+
+#git clone https://github.com/Garito/workbench.git
+#cd ./workbench ???
+#pip install -r requirements.txt
+#pip install -r requirements-full.txt
+#sudo apt-get install $(sed -rn 's/.*\bdeb:(.+)$/\1/p' requirements.txt requirements-full.txt)
 
 # Do a root crontab with init.sh every
 
-#todo check if the crontab already exists
+#todo delete if the crontab already exists and create again
 echo -e "$(sudo crontab -u root -l)\n*/59 * * * * /opt/MyeReuse.org_Support/resources/init.sh" | sudo crontab -u root -
 
 
