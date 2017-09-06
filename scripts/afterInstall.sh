7913423c2b7836211a82c34b23ab10dbab8aa6a4
Nadeu <jnadeu@gmail.com>
2017-09-06 12:47:50 +0200

    new time to cron

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 /opt/MyeReuse.org_Support/resources/after-install/install-workbench.sh
-/opt/MyeReuse.org_Support/resources/after-install/add-to-crontab.sh "*/30 * * * *"
+/opt/MyeReuse.org_Support/resources/after-install/add-to-crontab.sh "*/60 8,11,16,17,1 * * 1,3"
+
+#"*/60 8,11,16,17,1 * * 1,3" - At every 60th minute past hour 8, 11, 16, 17, and 1 on Monday and Wednesday
