f86e0dfa283239b1ffd64215559573395c2956c2
Nadeu <jnadeu@gmail.com>
2017-11-20 18:09:08 +0100

    disable devTools

--- app/main.js
+++ app/main.js
@@ -36,7 +36,7 @@ function createWindow () {
   })
 
   // Open devtools
-  winMain.webContents.openDevTools()
+  // winMain.webContents.openDevTools()
 
   winMain.on('closed', () => {
     winMain = null
