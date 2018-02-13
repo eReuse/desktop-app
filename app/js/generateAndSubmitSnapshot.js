03c8494092eddf234f720dfe75df394a6a8c4632
JNadeu <jnadeu@gmail.com>
2018-02-13 12:40:31 +0100

    fix diagnostic button

--- app/js/generateAndSubmitSnapshot.js
+++ app/js/generateAndSubmitSnapshot.js
@@ -1,44 +1,30 @@
 const spawnSync = require('child_process').spawnSync
-const chokidar = require('chokidar')
-const path = require('path')
-const fs = require('fs')
-const os = require('os')
 const notifier = require('node-notifier')
 const DeviceHub = require('../../resources/devicehub')
-const version = require('../../resources/.env.json').version
 // Documentation login and send json http://devicehub.ereuse.org/
 
 function generateAndSubmitSnapshot () {
-  // spawnSync('gksudo', ['-k', '/opt/MyeReuse.org_Support/eReuse.org-Workbench/workbench/scripts/erwb-devel'])
-  // spawnSync('sudo erwb')
+  //let result = spawnSync('sudo erwb --json')
+  let result = spawnSync('gksudo', ['-k', 'erwb --json'])
+  // let result = spawnSync('gksudo', ['-k', 'erwb', '--smart short' ,'--json'])
   console.log('Child exited wb finished')
-  const filePath = path.join(os.tmpdir(), '*.json')
-  chokidar.watch(filePath).on('add', path => {
-    let snapshot
-    try {
-      const text = fs.readFileSync(path, 'UTF-8')
-      snapshot = JSON.parse(text)
-      snapshot.snapshotSoftware = 'DesktopApp'
-      snapshot.versionDesktopApp = version || '0.1.0' //todo take automatically local version
-    } catch (err) {
-      console.error(err)
-      return
-    }
-    DeviceHub.post('events/devices/snapshot', snapshot).then(response => {
+  let snapshot = JSON.parse(result.stdout)
+  console.log(snapshot)
+  snapshot.snapshotSoftware = 'DesktopApp'
+  DeviceHub.post('/events/devices/snapshot', snapshot).then(response => {
       // tot ok el snapshot s'ha creat
-      console.log('Snapshot send successfully')
-      notifier.notify({
-        'title': 'Diagnostic',
-        'message': 'Snapshot send successfully ' + response
-      })
-    }).catch((err) => {
-      console.error(err)
-      notifier.notify({
-        'title': 'Diagnostic',
-        'message': 'Snapshot is not send correctly'
-      })
-      // todo error controls (no login, problems with snapshot, uuid check, etc..)
+    console.log('Snapshot send successfully')
+    notifier.notify({
+      'title': 'Diagnostic',
+      'message': 'Snapshot send successfully ' + response
     })
+  }).catch((err) => {
+    console.error(err)
+    notifier.notify({
+      'title': 'Diagnostic',
+      'message': 'Snapshot is not send correctly'
+    })
+      // todo error controls (no login, problems with snapshot, uuid check, etc..)
   })
 }
 
--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,4 @@
-// const generateAndSubmitSnapshot = require('./generateAndSubmitSnapshot')
+const generateAndSubmitSnapshot = require('./generateAndSubmitSnapshot')
 const page = require('../../resources/.env.json').pages.support
 window.$ = window.jQuery = require('jquery')
 window._ = require('lodash')
@@ -8,10 +8,8 @@ window.$('#main').html(`<iframe id="iframe" src="${page.url}"></iframe>`)
 window.$('#iframe').get(0).onload = () => {
   try {
     window.$('#iframe').get(0).contentDocument.head.children[0].text
-    console.log('loaded')
   } catch(err){
     changeUrl()
-    console.log('no')
   }
 }
 
@@ -21,11 +19,7 @@ function changeUrl () {
 }
 
 // Executes the workbench
-// document.getElementById('botoDiag').addEventListener('click', generateAndSubmitSnapshot)
-/* Button Diag Code <br>
-  <div class="text-center">
-    <button id='botoDiag' class="btn btn-primary btn-lg">Diagnostic</button>
-  </div> */
+document.getElementById('botoDiag').addEventListener('click', generateAndSubmitSnapshot)
 
 // Access to DOM, change or put
 //document.title = titleHTML
