52c6a6b97cdedacf7e4fff255ffc6348415ff808
JNadeu <jnadeu@gmail.com>
2018-02-16 20:03:42 +0100

    installation workbench10

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,10 +2,12 @@
 # You need to run this file with sudo or root user
 
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )" # from https://stackoverflow.com/a/4774063
-# Installation path
+# Installation path, rlly needed??
 INSTALL_DIR='/opt/MyeReuse.org_Support/'
 # Latest LTS Node version
 NODE_VERSION='7.10.1'
+# Workbench version you want to install
+WB_VERSION='1.0'
 
 echo $SCRIPT_DIR
 
@@ -24,21 +26,19 @@ export PATH="/opt/MyeReuse.org_Support/n/bin:$PATH"
 
 # Local folder
 mkdir -pm 755 /opt/MyeReuse.org_Support
-cd ${INSTALL_DIR}
+#################cd ${INSTALL_DIR}
 
 # node will install in /opt/MyeReuse.org_Support/node_modules
 npm install semver request request-promise promise node-linux lodash
 
-# cd ${INSTALL_DIR}/eReuse.org-Workbench
-# pip install setuptools enum34 python-dateutil pySMART pyudev tqdm requests lxml gnupg celery redis  # Workbench dependencies
-
 echo "Installing Workbench"
 # Clone workbench files
-git clone --recursive https://github.com/eReuse/workbench.git || true
+# git clone --recursive https://github.com/eReuse/workbench.git || true
+
 # Execute workbench installation
-cd workbench
-chmod 755 scripts/install.sh
-./script/install.sh ${INSTALL_DIR}/workbnech
+pip3 install ereuse-workbench ${WB_VERSION}
+wget -P /usr/local/sbin https://raw.githubusercontent.com/eReuse/workbench/master/scripts/erwb
+chmod 755 /usr/local/sbin/erwb
 
 node /opt/MyeReuse.org_Support/resources/init.node.js
 
